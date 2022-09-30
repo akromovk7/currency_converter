1c0fac7bd39a1a37c0312b39e2425c4246b67c93
akromovkamoliddin <akromovkwork@gmail.com>
2022-09-30 17:29:40 +0500

    Splash Screen Page Finished

--- lib/screens/view/pages/onboarding_page.dart
+++ lib/screens/view/pages/onboarding_page.dart
@@ -1,10 +1,39 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class OnBoardingView extends StatelessWidget {
+import 'package:currency_converter/screens/cubit/main_cubit.dart';
+import 'package:currency_converter/screens/state/main_state.dart';
+import 'package:flutter/material.dart';
+import 'package:currency_converter/core/base/size_extension.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+
+class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
 
   @override
+  State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(milliseconds: 1500), () {
+      context.read<MainCubit>().changeState(HomeState());
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.amber,);
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          height: context.height,
+          width: context.width,
+          child: Lottie.asset("assets/lottie/dollar.json"),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/service/currency_service.dart
@@ -0,0 +1,3 @@
+class Constant {
+  static String myApi = 'https://nbu.uz/en/exchange-rates/json/';
+}
